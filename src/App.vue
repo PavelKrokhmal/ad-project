6c05c44fcc19d2fec27a7eba0ce7b261f8c55ff3
Pavel Krokhmal <pavel.krokhmal.mn@outlook.com>
2021-05-23 07:07:53 +0300

    Finish toolbar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,26 @@
 <template>
   <v-app>
-    <h1>Hello, Vue!</h1>
-  </v-app>
+    <v-navigation-drawer app temporary>
+    <!-- -->
+    </v-navigation-drawer>
+
+    <v-app-bar absolute dark color="orange darken-1">
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-toolbar-title>Ad application</v-toolbar-title>
+      <v-spacer></v-spacer>
+  
+      <v-toolbar-items>
+        <v-btn text>One link</v-btn>
+        <v-btn text>Two link</v-btn>
+        <v-btn text>Third link</v-btn>
+      </v-toolbar-items>
+    </v-app-bar>
+
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+
+    </v-app>
 </template>
 
 <script>
